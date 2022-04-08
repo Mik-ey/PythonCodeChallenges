9c928fe7a6b80cc4f05b67ee1cbd852b4bbe9231
Mik-ey <work.michal.kowalczyk@gmail.com>
2022-04-08 20:58:28 +0200

    Add solution to average_race_time.py

--- average_race_time.py
+++ average_race_time.py
@@ -4,22 +4,39 @@
 import re
 import datetime
 
+
 def get_data():
     """Return content from the 10k_racetimes.txt file"""
-    with open('10k_racetimes.txt', 'rt') as file:
+    with open("10k_racetimes.txt", "rt") as file:
         content = file.read()
     return content
 
+
 def get_rhines_times():
     """Return a list of Jennifer Rhines' race times"""
-    races = get_data()
-    pass
+    time_pattern = r"\d+:\d+(\.\d+)?"
+    _, *races = get_data().split("\n")
+    return [
+        re.search(time_pattern, race).group()
+        for race in races
+        if "Jennifer Rhines" in race
+    ]
+
 
 def get_average():
     """Return Jennifer Rhines' average race time in the format:
-       mm:ss:M where :
-       m corresponds to a minutes digit
-       s corresponds to a seconds digit
-       M corresponds to a milliseconds digit (no rounding, just the single digit)"""
-    racetimes = get_rhines_times()
-    pass
+    mm:ss:M where :
+    m corresponds to a minutes digit
+    s corresponds to a seconds digit
+    M corresponds to a milliseconds digit (no rounding, just the single digit)"""
+    sum_time = datetime.timedelta()
+    rhines_times = get_rhines_times()
+    for time in rhines_times:
+        m, s, *ms = re.split(r"[\.:]+", time)
+
+        sum_time += datetime.timedelta(
+            minutes=int(m), seconds=int(s), milliseconds=int(ms[0]) if ms else 0
+        )
+    _, m, s, *ms = re.split(r"[\.:]+", str(sum_time / len(rhines_times)))
+
+    return f"{m}:{s}.{ms[0][0] if ms else 0}"
