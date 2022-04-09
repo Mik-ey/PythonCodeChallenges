4784ca21aef63d9162e79578757696b0e1cf5843
Mik-ey <work.michal.kowalczyk@gmail.com>
2022-04-09 23:34:45 +0200

    Implement solution for pairwise_offset

--- pairwise_offset.py
+++ pairwise_offset.py
@@ -1,2 +1,11 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+def pairwise_offset(sequence, fillvalue="*", offset=0):
+    first_elements = [
+        sequence[i] if i < len(sequence) else fillvalue
+        for i in range(len(sequence) + offset)
+    ]
+    second_elements = [
+        fillvalue if i < offset else sequence[i - offset]
+        for i in range(len(sequence) + offset)
+    ]
+
+    return list(zip(first_elements, second_elements))
