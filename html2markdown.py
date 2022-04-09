21b92c26a8dfe46e7caf1f5e16a059132bea17b3
Mik-ey <work.michal.kowalczyk@gmail.com>
2022-04-09 19:44:39 +0200

    html2markdown - Enable converting italics

--- html2markdown.py
+++ html2markdown.py
@@ -1,4 +1,9 @@
+import re
+
 
 def html2markdown(html):
-    '''Take in html text as input and return markdown'''
-    pass
+    """Take in html text as input and return markdown"""
+    # Convert italics
+    markdown_text = re.sub(r"<[/]?em>", "*", html)
+
+    return markdown_text
