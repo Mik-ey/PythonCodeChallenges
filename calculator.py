0fea6dd5b1bc43c492ec8e1dc21d8557e56ae83a
Mik-ey <work.michal.kowalczyk@gmail.com>
2022-04-13 22:22:43 +0200

    Implement context manager in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,2 +1,11 @@
 class Calculator:
-    pass
+    def __init__(self, *args):
+        self.handled_exceptions = args
+        self.error = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.error = exc_type
+        return isinstance(exc_type, self.handled_exceptions)
