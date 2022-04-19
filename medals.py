5c9e7a5b408b573545d80ea96997449d4f9c3ba9
Mik-ey <work.michal.kowalczyk@gmail.com>
2022-04-20 00:14:21 +0200

    Solve get_medals challenge ]

--- medals.py
+++ medals.py
@@ -24,4 +24,29 @@ medal = namedtuple(
 
 def get_medals(**kwargs):
     """Return a list of medal namedtuples"""
-    pass
+
+    if not set(kwargs.keys()).issubset(medal._fields):
+        raise Exception("Invalid key word arguments")
+
+    temp_df = olympics.copy()
+    for k, v in kwargs.items():
+        temp_df = temp_df[temp_df[k] == v]
+
+    results = []
+    for _, row in temp_df.iterrows():
+        results.append(
+            medal(
+                City=row.City,
+                Edition=row.Edition,
+                Sport=row.Sport,
+                Discipline=row.Discipline,
+                Athlete=row.Athlete,
+                NOC=row.NOC,
+                Gender=row.Gender,
+                Event=row.Event,
+                Event_gender=row.Event_gender,
+                Medal=row.Medal,
+            )
+        )
+
+    return results
